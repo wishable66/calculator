b66092c5a685fc47ba85871eff533e4d9cc0f2bf
Wishable <gjbbub05@gmail.com>
2023-03-28 17:28:50 -0600

    Complete calculator functionality

--- public/index.js
+++ public/index.js
@@ -14,9 +14,6 @@ const divide = (array) => {
         ? array.reduce((accumulator, nextItem) => accumulator / nextItem)
         : 0;
 };
-let firstNumber;
-let secondNumber;
-let operator;
 function operate(firstNumber, secondNumber, operator) {
     if (isNaN(firstNumber) || isNaN(secondNumber))
         return;
@@ -29,3 +26,111 @@ function operate(firstNumber, secondNumber, operator) {
     if (operator === '/')
         return divide([firstNumber, secondNumber]);
 }
+const display = document.querySelector('.display-text');
+function updateDisplay(text, set) {
+    return set ? (display.textContent = text) : (display.textContent += text);
+}
+function resetValues() {
+    return ((firstNumber = ''),
+        (secondNumber = ''),
+        (operator = ''),
+        (result = 0),
+        (display.textContent = ''));
+}
+function checkForZero(firstNumber, secondNumber, operator) {
+    if (operator === '/' && secondNumber === 0)
+        return 'ERROR';
+    else
+        return 'good';
+}
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+let result = 0;
+const operators = ['+', '-', '/', '*'];
+const btns = document.querySelectorAll('.buttons > *');
+btns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const target = e.target;
+        const buttonValue = target.attributes[1].value;
+        const isNumber = !isNaN(Number(buttonValue)) || buttonValue === '.';
+        if (isNumber && !operator) {
+            updateDisplay(buttonValue);
+            firstNumber += buttonValue;
+        }
+        if (isNumber && operator) {
+            updateDisplay(buttonValue);
+            secondNumber += buttonValue;
+        }
+        if (operators.includes(buttonValue)) {
+            updateDisplay(buttonValue);
+            if (secondNumber) {
+                firstNumber = parseInt(firstNumber);
+                secondNumber = parseInt(secondNumber);
+                if (checkForZero(firstNumber, secondNumber, operator) === 'ERROR') {
+                    resetValues();
+                    return (display.textContent = 'ERROR');
+                }
+                result = operate(firstNumber, secondNumber, operator);
+                firstNumber = String(result);
+                secondNumber = '';
+                return (operator = buttonValue);
+            }
+            operator = buttonValue;
+        }
+        if (buttonValue === '=') {
+            if (firstNumber === '' || secondNumber === '' || operator === '')
+                return;
+            firstNumber = parseInt(firstNumber);
+            secondNumber = parseInt(secondNumber);
+            if (checkForZero(firstNumber, secondNumber, operator) === 'ERROR') {
+                resetValues();
+                return (display.textContent = 'ERROR');
+            }
+            result = operate(firstNumber, secondNumber, operator);
+            updateDisplay(String(result), true);
+            firstNumber = String(result);
+            secondNumber = '';
+            operator = '';
+        }
+        if (buttonValue === 'reset') {
+            return resetValues();
+        }
+        if (buttonValue === 'backspace') {
+            if (firstNumber === '' && secondNumber === '' && operator === '')
+                return;
+            if (firstNumber !== '' && secondNumber === '' && operator === '') {
+                if (firstNumber.length === 1) {
+                    resetValues();
+                }
+                else {
+                    const numberRemoved = display.textContent.slice(0, -1);
+                    updateDisplay(numberRemoved, true);
+                    firstNumber = numberRemoved;
+                }
+            }
+            if (firstNumber !== '' && secondNumber !== '' && operator !== '') {
+                if (secondNumber.length === 1) {
+                    const textRemoved = display.textContent.slice(0, -1);
+                    const numberRemoved = secondNumber.slice(0, -1);
+                    console.log(textRemoved);
+                    updateDisplay(textRemoved, true);
+                    console.log(numberRemoved);
+                    secondNumber = numberRemoved;
+                }
+                else {
+                    const textRemoved = display.textContent.slice(0, -1);
+                    const numberRemoved = secondNumber.slice(0, -1);
+                    console.log(numberRemoved);
+                    updateDisplay(textRemoved, true);
+                    secondNumber = numberRemoved;
+                }
+            }
+            console.log({
+                firstNumber,
+                secondNumber,
+                operator,
+            });
+        }
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -18,10 +18,6 @@ const divide = (array: number[]): number => {
     : 0;
 };
 
-let firstNumber: number;
-let secondNumber: number;
-let operator: string;
-
 function operate(firstNumber: number, secondNumber: number, operator: string) {
   if (isNaN(firstNumber) || isNaN(secondNumber)) return;
 
@@ -30,3 +26,125 @@ function operate(firstNumber: number, secondNumber: number, operator: string) {
   if (operator === '*') return multiply([firstNumber, secondNumber]);
   if (operator === '/') return divide([firstNumber, secondNumber]);
 }
+
+const display = document.querySelector('.display-text');
+
+function updateDisplay(text: string, set?: boolean) {
+  return set ? (display.textContent = text) : (display.textContent += text);
+}
+
+function resetValues() {
+  return (
+    (firstNumber = ''),
+    (secondNumber = ''),
+    (operator = ''),
+    (result = 0),
+    (display.textContent = '')
+  );
+}
+
+function checkForZero(firstNumber, secondNumber, operator) {
+  if (operator === '/' && secondNumber === 0) return 'ERROR';
+  else return 'good';
+}
+
+let firstNumber: any = '';
+let secondNumber: any = '';
+let operator: string = '';
+let result: number = 0;
+const operators: string[] = ['+', '-', '/', '*'];
+
+const btns = document.querySelectorAll('.buttons > *');
+
+btns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const target = e.target as HTMLButtonElement;
+    const buttonValue = target.attributes[1].value;
+    const isNumber: number | boolean =
+      !isNaN(Number(buttonValue)) || buttonValue === '.';
+    if (isNumber && !operator) {
+      updateDisplay(buttonValue);
+      firstNumber += buttonValue;
+    }
+
+    if (isNumber && operator) {
+      updateDisplay(buttonValue);
+      secondNumber += buttonValue;
+    }
+
+    if (operators.includes(buttonValue)) {
+      updateDisplay(buttonValue);
+
+      if (secondNumber) {
+        firstNumber = parseInt(firstNumber);
+        secondNumber = parseInt(secondNumber);
+        if (checkForZero(firstNumber, secondNumber, operator) === 'ERROR') {
+          resetValues();
+          return (display.textContent = 'ERROR');
+        }
+        result = operate(firstNumber, secondNumber, operator);
+        firstNumber = String(result);
+        secondNumber = '';
+        return (operator = buttonValue);
+      }
+      operator = buttonValue;
+    }
+
+    if (buttonValue === '=') {
+      if (firstNumber === '' || secondNumber === '' || operator === '') return;
+      firstNumber = parseInt(firstNumber);
+      secondNumber = parseInt(secondNumber);
+      if (checkForZero(firstNumber, secondNumber, operator) === 'ERROR') {
+        resetValues();
+        return (display.textContent = 'ERROR');
+      }
+      result = operate(firstNumber, secondNumber, operator);
+      updateDisplay(String(result), true);
+
+      firstNumber = String(result);
+      secondNumber = '';
+      operator = '';
+    }
+
+    if (buttonValue === 'reset') {
+      return resetValues();
+    }
+
+    if (buttonValue === 'backspace') {
+      if (firstNumber === '' && secondNumber === '' && operator === '') return;
+
+      if (firstNumber !== '' && secondNumber === '' && operator === '') {
+        if (firstNumber.length === 1) {
+          resetValues();
+        } else {
+          const numberRemoved = display.textContent.slice(0, -1);
+          updateDisplay(numberRemoved, true);
+          firstNumber = numberRemoved;
+        }
+      }
+
+      if (firstNumber !== '' && secondNumber !== '' && operator !== '') {
+        if (secondNumber.length === 1) {
+          const textRemoved = display.textContent.slice(0, -1);
+          const numberRemoved = secondNumber.slice(0, -1);
+          console.log(textRemoved);
+          updateDisplay(textRemoved, true);
+          console.log(numberRemoved);
+          secondNumber = numberRemoved;
+        } else {
+          const textRemoved = display.textContent.slice(0, -1);
+          const numberRemoved = secondNumber.slice(0, -1);
+          console.log(numberRemoved);
+          updateDisplay(textRemoved, true);
+          secondNumber = numberRemoved;
+        }
+      }
+
+      console.log({
+        firstNumber,
+        secondNumber,
+        operator,
+      });
+    }
+  });
+});
